9348fdd337b83217ec16f79c1cb126af22f65616
dhrvmlk <malikdhruv014@gmail.com>
2024-05-26 08:20:44 +0530

    feat: added implementation of method to read acl

--- ACL.cpp
+++ ACL.cpp
@@ -1,3 +1,44 @@
+#include <iostream>
+#include <string>
+#include "acl.hpp"
+
+// Function to read ACL
+std::string ACL::ReadACL()
+{
+    // Get file path from user
+    std::cout << "Enter the file path: ";
+    std::cin.ignore();
+    std::getline(std::cin, filename);
+
+    // Construct command for icacls and execute it using popen
+    std::string command = "icacls \"" + filename + "\"";
+    FILE *pipe = popen(command.c_str(), "r");
+    if (!pipe)
+    {
+        // Throw an exception if command execution fails
+        throw std::runtime_error("Error executing icacls command");
+    }
+
+    // Read command output into result string
+    std::string result = "";
+    while (fgets(buffer, 128, pipe))
+    {
+        result += buffer;
+    }
+
+    // Close pipe and check the status
+    int status = pclose(pipe);
+    if (status != 0)
+    {
+        // Throw an exception if command execution fails
+        throw std::runtime_error("icacls command failed with error code " + std::to_string(status));
+    }
+
+    // Print result and return the command
+    std::cout << result << std::endl;
+    return command;
+}
+
 // Function to grant permission
 void ACL::GrantPermission()
 {
